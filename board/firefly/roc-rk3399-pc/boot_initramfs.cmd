2e61b3a2b97476762bf38089d47ac8b358c12d74
Suniel Mahesh <sunil@amarulasolutions.com>
2020-06-19 23:33:15 +0530

    board: roc-rk3399-pc: more changes
    Signed-off-by: Suniel Mahesh <sunil@amarulasolutions.com>

--- board/firefly/roc-rk3399-pc/boot_initramfs.cmd
+++ board/firefly/roc-rk3399-pc/boot_initramfs.cmd
@@ -1,4 +1,4 @@
 setenv bootargs earlycon=uart8250,mmio32,0xff1a0000 root=/dev/ram rootwait rw
 
-sf read $kernel_addr_r 0x142000 0xe00000
+sf read $kernel_addr_r 0x3FE000 0xc00000
 bootm $kernel_addr_r
--- board/firefly/roc-rk3399-pc/post-build.sh
+++ board/firefly/roc-rk3399-pc/post-build.sh
@@ -13,18 +13,22 @@ $MKIMAGE -A arm -O linux -T script -C none -d $BOARD_DIR/boot_initramfs.cmd $BIN
 # copy spi loader
 install -m 0644 -D $BOARD_DIR/rk3399_loader_spinor_v1.15.114.bin $BINARIES_DIR
 
+# create spi idbloader.img
+$MKIMAGE -n rk3399 -T rkspi -d $BINARIES_DIR/u-boot-tpl.bin:$BINARIES_DIR/u-boot-spl.bin $BINARIES_DIR/idbloader.img
+
 # create single bootable spi image
 #
-#      0x0:	idbloader.img
-#  0x40000:	u-boot.itb
-# 0x142000:	uImage.itb
-# 0xFFE000:	boot_initramfs.scr
+#       0x0:	idbloader.img
+#   0x60000:	u-boot.itb
+#  0x3F8000:    env
+#  0x3FE000:	uImage.itb
+#  0xFFE000:	boot_initramfs.scr
 (cd $BINARIES_DIR
 
-  dd if=idbloader.img of=idbloader-pad.img bs=256K conv=sync
+  dd if=idbloader.img of=idbloader-pad.img bs=384K conv=sync
   cat idbloader-pad.img u-boot.itb > u-boot-idbloader.img
 
-  dd if=u-boot-idbloader.img of=u-boot-idbloader-pad.img bs=1288K conv=sync
+  dd if=u-boot-idbloader.img of=u-boot-idbloader-pad.img bs=4088K conv=sync
   cat u-boot-idbloader-pad.img uImage.itb > u-boot-uImage.img
 
   dd if=u-boot-uImage.img of=u-boot-uImage-pad.img bs=16376K conv=sync
